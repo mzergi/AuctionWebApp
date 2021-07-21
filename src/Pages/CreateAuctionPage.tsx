feb4ca7f8c9363e0a2f918864aa60597c1ca1733
u26p65 <Mate.Zergi@avl.com>
2021-07-21 16:24:41 +0200

    Keeps working on create auction page.

--- src/Pages/CreateAuctionPage.tsx
+++ src/Pages/CreateAuctionPage.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import { useAppSelector, useAppDispatch } from '../App/hooks';
-import { Container, Row, Col, Form, Modal, Button } from "react-bootstrap";
-import { store } from '../App/store';
+import {useAppSelector, useAppDispatch} from '../App/hooks';
+import {Container, Row, Col, Form, Modal, Button} from "react-bootstrap";
+import {store} from '../App/store';
 import axios from "axios";
 import * as signalR from "@microsoft/signalr";
 import {Product, Category, AuctionItem, User, Bid} from "../Model/auction_types";
@@ -21,6 +21,8 @@ export default function CreateAuctionPage() {
     const createUrl = "http://localhost:5000/api/auctionspage/auctions/create";
     const baseUrl = "http://localhost:5000/api/";
 
+    const [newProductModalIsOpen, setNewProductModalIsOpen] = useState(false);
+
     //form states
     const [inputProduct, setProduct] = useState({} as Product);
     const [description, setDescription] = useState("");
@@ -29,7 +31,8 @@ export default function CreateAuctionPage() {
     const [highlighted, setHighlighted] = useState(false);
     const [startingPrice, setStartingPrice] = useState(0);
     const [productCategory, setProductCategory] = useState({} as Category);
-    const [productName, setProductName] = useState("");
+    const [newProductName, setNewProductName] = useState("");
+    const [newProductCategory, setNewProductCategory] = useState({} as Category);
 
     const [loadedProducts, setLoadedProducts] = useState([] as Product[]);
     const [loadedCategories, setLoadedCategories] = useState([] as Category[]);
@@ -48,11 +51,20 @@ export default function CreateAuctionPage() {
         )()
     }, [])
 
-    const CreateAuction = async () => {
+    function openNewProductModal()
+    {
+        setNewProductModalIsOpen(true);
+    }
+    function closeNewProductModal()
+    {
+        setNewProductModalIsOpen(false);
+    }
+    // TODO: TEST IF IT WORKS
+    async function CreateAuction(){
         var toCreate: AuctionItem = {
             id: {} as number,
             topBidder: {} as User,
-            name: productName,
+            name: inputProduct.name,
             product: inputProduct,
             description: description,
             startOfAuction: startOfAuction,
@@ -63,30 +75,64 @@ export default function CreateAuctionPage() {
             topBid: {} as Bid,
             createdBy: loggedInUser
         };
-        await axios.post(createUrl, toCreate);
+        var result = await axios.post(createUrl, toCreate);
+        console.log(result.data);
     }
-
-    const CreateProduct = () => {
-        var createProduct: Product = {
+    // TODO: MAKE ASYNC, POST TO DB, LOAD BACK
+    function CreateProduct() {
+        let createProduct: Product = {
             id: {} as number,
-            name: productName,
-            category: productCategory,
+            name: newProductName,
+            category: newProductCategory,
             imagePath: "",
-            categoryID: productCategory.id
+            categoryID: newProductCategory.id
         };
-
-        setProduct(createProduct);
+        setProduct({...createProduct});
+        setProductCategory({...newProductCategory});
+        let loaded = loadedProducts;
+        loaded.push(createProduct);
+        setLoadedProducts([...loaded]);
+        closeNewProductModal();
+        console.log(createProduct);
+        console.log(inputProduct);
+        console.log(productCategory);
     }
 
-    return(
+    async function CategorySelected(categoryId: number) {
+        var category = await axios.get(baseUrl + "auctionspage/categories/" + categoryId);
+        var productsOfCategory = await axios.get(baseUrl + "auctionspage/products/category/" + categoryId);
+
+        let old = loadedProducts;
+        old = productsOfCategory.data;
+
+        setLoadedProducts([...old]);
+        setProductCategory({...category.data});
+        console.log(productCategory);
+        console.log(loadedProducts);
+    }
+
+    function SetNewProductCategoryById(id: number){
+        let category = loadedCategories.find(c => c.id === id);
+
+        setNewProductCategory(category as Category);
+    }
+
+    function setProductFromId(id: number){
+        let product = loadedProducts.find(p => p.id == id);
+
+        setProduct(product as Product);
+    }
+
+    return (
         <Container>
             <Row>
                 <Col lg={5} style={{marginTop: "8%"}}>
-                <h3>Create new auction</h3>
+                    <h3>Create new auction</h3>
                     <Form style={{marginTop: "10%"}} inline>
                         <Form.Group>
                             <Form.Label className="my-1 mr-3">Category</Form.Label>
-                            <Form.Control as="select" className="my-1 mr-sm-3" custom>
+                            <Form.Control as="select" className="my-1 mr-sm-3" custom
+                                          onChange={async(e) => {await CategorySelected(parseInt(e.target.value))}}>
                                 <option value="0">Choose category</option>
                                 {loadedCategories.map((category) => (
                                     <option value={category.id}>{category.name}</option>
@@ -97,57 +143,96 @@ export default function CreateAuctionPage() {
                     <Form>
                         <Form style={{marginTop: "5%"}}>
                             <Form.Group>
-                                <Form.Label>Product name</Form.Label>
-                                <Form.Control type="text" maxLength={100}
-                                value = {productName}
-                                onChange = {(e: React.ChangeEvent<HTMLInputElement>) =>  {
-                                    setProductName(e.currentTarget.value);
-                                    }}></Form.Control>
+                                <Form.Label>Select Product</Form.Label>
+                                <Row>
+                                    <Col>
+                                        <Form.Control as="select" onChange={e => {setProductFromId(parseInt(e.currentTarget.value))}} value = {inputProduct.id} custom>
+                                            <option value="0">Choose Product</option>
+                                            {loadedProducts.map((product) => (
+                                                <option value={product.id}>{product.name}</option>
+                                            ))}
+                                        </Form.Control>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="success" onClick={openNewProductModal}>New product</Button>
+                                    </Col>
+                                </Row>
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label>Description (max. 300 characters)</Form.Label>
                                 <Form.Control as="textarea" maxLength={300} value={description}
-                                    onChange= {(e: React.ChangeEvent<HTMLInputElement>)=> setDescription(e.currentTarget.value)}/>
+                                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDescription(e.currentTarget.value)}/>
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label>Start of auction</Form.Label>
-                                <Form.Control type = "date"
-                                value = {moment(new Date(startOfAuction)).format('YYYY-MM-DD')}
-                                onChange = {(e: React.ChangeEvent<HTMLInputElement>)=> {
-                                    var date = new Date(moment(e.currentTarget.value).format('YYYY-MM-DD'));
-                                    setStart(date);
-                                }}/>
+                                <Form.Control type="date"
+                                              value={moment(new Date(startOfAuction)).format('YYYY-MM-DD')}
+                                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                  var date = new Date(moment(e.currentTarget.value).format('YYYY-MM-DD'));
+                                                  setStart(date);
+                                              }}/>
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label>End of auction</Form.Label>
-                                <Form.Control type = "date"
-                                value = {moment(new Date(endOfAuction)).format('YYYY-MM-DD')}
-                                onChange = {(e: React.ChangeEvent<HTMLInputElement>)=> {
-                                    var date = new Date(moment(e.currentTarget.value).format('YYYY-MM-DD'));
-                                    setEnd(date);
-                                }}/>
+                                <Form.Control type="date"
+                                              value={moment(new Date(endOfAuction)).format('YYYY-MM-DD')}
+                                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                  var date = new Date(moment(e.currentTarget.value).format('YYYY-MM-DD'));
+                                                  setEnd(date);
+                                              }}/>
                             </Form.Group>
                             <Form.Group>
-                                <Form.Check type="checkbox" label="Highlight auction" checked = {highlighted}
-                                onChange = {(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setHighlighted(e.target.checked);
-                                }}/>
+                                <Form.Check type="checkbox" label="Highlight auction" checked={highlighted}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                setHighlighted(e.target.checked);
+                                            }}/>
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label>Starting price</Form.Label>
-                                <Form.Control type = "number" value = {startingPrice}
-                                onChange = {(e: React.ChangeEvent<HTMLInputElement>) => {
-                                    setStartingPrice(parseInt(e.currentTarget.value));
-                                } }></Form.Control>
+                                <Form.Control type="number" value={startingPrice}
+                                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                  setStartingPrice(parseInt(e.currentTarget.value));
+                                              }}></Form.Control>
                             </Form.Group>
-                            <Button variant="success">Create!</Button>
+                            <Button variant="success" onClick={CreateAuction}>Create!</Button>
                         </Form>
                     </Form>
                 </Col>
                 <Col>
-                    
+
                 </Col>
             </Row>
+            <Modal show={newProductModalIsOpen} onHide={closeNewProductModal}>
+                <Modal.Header>
+                    <Modal.Title>Create new Product</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" value={newProductName}
+                                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                              setNewProductName(e.currentTarget.value);
+                                          }}>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control as="select" custom
+                                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {SetNewProductCategoryById(parseInt(e.currentTarget.value))}}>
+                                <option value="0">Choose category</option>
+                                {loadedCategories.map((category) => (
+                                    <option value={category.id}>{category.name}</option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={closeNewProductModal}>Close</Button>
+                    <Button variant="success" onClick={CreateProduct}>Create</Button>
+                </Modal.Footer>
+            </Modal>
         </Container>
     )
 }
